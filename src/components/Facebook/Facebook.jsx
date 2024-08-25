6610c8de71c013663d3dc7f1b86d3462f41c4b9b
Raushan Kumar <raushankcse@gmail.com>
2024-08-25 15:21:08 +0530

    design

--- src/components/Facebook/Facebook.jsx
+++ src/components/Facebook/Facebook.jsx
@@ -1,64 +1,65 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons"; // Import the Facebook icon
 
-
-const Facebook  = () =>{
+const Facebook = () => {
   const navigate = useNavigate();
-  const handleButtonClick = ()=> {
-    navigate('/twitter');
+
+  const handleButtonClick = () => {
+    // Store a flag in localStorage to check if the user is returning from Facebook
+    localStorage.setItem("redirectedToFacebook", "true");
+
+    // Attempt to open the Facebook page in the app
+    window.location.href = "fb://page/SCellGecSiwan";
+
+    // Fallback to the Facebook web profile if the app is not installed
+    setTimeout(() => {
+      window.location.href = "https://www.facebook.com/SCellGecSiwan?mibextid=ZbWKwL";
+    }, 1000); // Wait 1 second before falling back
   };
-  
+
+  useEffect(() => {
+    // Check if the user was redirected to Facebook and has returned
+    const redirected = localStorage.getItem("redirectedToFacebook");
+    if (redirected) {
+      // Clear the flag from local storage
+      localStorage.removeItem("redirectedToFacebook");
+
+      // Navigate to the Twitter page after returning
+      navigate("/twitter");
+    }
+  }, [navigate]);
+
   return (
     <div className="container">
-
       <div className="header">
         <div className="logo">
           <div className="yellow-logo">
-            <img className="logoImg" src="/logo.png" alt="" />
-          </div> 
+            <img className="logoImg" src="/logo.png" alt="Logo" />
+          </div>
         </div>
         <div className="startup">
           <h1 className="startup-text">Startup Cell</h1>
           <h1 className="college-name">Government Engineering College Siwan</h1>
         </div>
         <div className="quiz">
-          <img className="quiz-img" src="/quiz.png" alt="" />
+          <img className="quiz-img" src="/quiz.png" alt="Quiz" />
         </div>
       </div>
 
-
-      <div class="background-container">
-        <div class="large-circle"></div>
-        <div class="small-circle"></div>
-        <img src="/quiz2.png" alt="Image outside circle" class="circle-image"/>
-        <p class="circle-text">To start the quiz follow the <br />following steps</p>
-        <button onClick={handleButtonClick} class="circle-button"><span></span>Follow <span className="symbol">&gt;</span></button>
-     </div>
-
-      {/* <div className="footer">
-        <div class="large-circle">
-          <div class="small-circle">
-            <img className="circle-image" src="/quiz2.png" alt="" />
-            <h1 className="circle-text">To start the quiz follow <br /> the following steps</h1>
-            <button className="circle-button">Next</button>
-          </div>
-        </div>
-      </div> */}
-
-        {/* <div className="content">
-          <div className="goal-image">
-            <img className="quiz-img2" src="/quiz2.png" alt="" />
-          </div>
-          <div className="start">
-            <h1 className="quiz-start-text">To start the quiz follow <br /> the following steps</h1>
-          </div>
-          <div className="button">
-            <button className="btn">Next</button>
-          </div>
-        </div> */}
-
+      <div className="background-container">
+        <div className="large-circle"></div>
+        <div className="small-circle"></div>
+        <img src="/quiz2.png" alt="Image outside circle" className="circle-image" />
+        <p className="circle-text">To start the quiz follow the <br /> following steps</p>
+        <button onClick={handleButtonClick} className="circle-button">
+          <FontAwesomeIcon icon={faFacebook} style={{ marginRight: '8px' }} /> {/* Facebook icon */}
+          Follow <span className="symbol">&gt;</span>
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default Facebook;
--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,64 +1,42 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-
-const Quiz  = () =>{
+const Quiz = () => {
   const navigate = useNavigate();
-  const handleButtonClick = ()=> {
-    navigate('/form');
+
+  const handleButtonClick = () => {
+    // Open the Google Form link
+    window.location.href = "https://forms.gle/zz7ekEd3PbhgskBS6";
   };
-  
+
   return (
     <div className="container">
-
       <div className="header">
         <div className="logo">
           <div className="yellow-logo">
-            <img className="logoImg" src="/logo.png" alt="" />
-          </div> 
+            <img className="logoImg" src="/logo.png" alt="Logo" />
+          </div>
         </div>
         <div className="startup">
           <h1 className="startup-text">Startup Cell</h1>
           <h1 className="college-name">Government Engineering College Siwan</h1>
         </div>
         <div className="quiz">
-          <img className="quiz-img" src="/quiz.png" alt="" />
+          <img className="quiz-img" src="/quiz.png" alt="Quiz" />
         </div>
       </div>
 
-
-      <div class="background-container">
-        <div class="large-circle"></div>
-        <div class="small-circle"></div>
-        <img src="/quiz2.png" alt="Image outside circle" class="circle-image"/>
-        <p class="circle-text">To start the quiz follow the <br />following steps</p>
-        <button onClick={handleButtonClick} class="circle-button"><span></span>Follow <span className="symbol">&gt;</span></button>
-     </div>
-
-      {/* <div className="footer">
-        <div class="large-circle">
-          <div class="small-circle">
-            <img className="circle-image" src="/quiz2.png" alt="" />
-            <h1 className="circle-text">To start the quiz follow <br /> the following steps</h1>
-            <button className="circle-button">Next</button>
-          </div>
-        </div>
-      </div> */}
-
-        {/* <div className="content">
-          <div className="goal-image">
-            <img className="quiz-img2" src="/quiz2.png" alt="" />
-          </div>
-          <div className="start">
-            <h1 className="quiz-start-text">To start the quiz follow <br /> the following steps</h1>
-          </div>
-          <div className="button">
-            <button className="btn">Next</button>
-          </div>
-        </div> */}
-
+      <div className="background-container">
+        <div className="large-circle"></div>
+        <div className="small-circle"></div>
+        <img src="/quiz2.png" alt="Image outside circle" className="circle-image" />
+        <p className="circle-text">To start the quiz follow the <br /> following steps</p>
+        <button onClick={handleButtonClick} className="circle-button">
+          Attempt <span className="symbol">&gt;</span>
+        </button>
+      </div>
     </div>
   );
-}
+};
 
 export default Quiz;
